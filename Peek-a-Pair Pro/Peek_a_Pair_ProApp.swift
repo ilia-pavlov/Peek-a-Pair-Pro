3e7dc5458207c32abffa6f9c381f1230d17c271a
ilia Pavlov <68978403+ilia-pavlov@users.noreply.github.com>
2023-08-17 21:42:53 -0400

    Initial Commit

---
+++ Peek-a-Pair Pro/Peek_a_Pair_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peek_a_Pair_ProApp.swift
+//  Peek-a-Pair Pro
+//
+//  Created by Ilia Pavlov on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Peek_a_Pair_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
