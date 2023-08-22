56ea6ce63702a87ce9100308f8cd6bd04a333c78
ilia Pavlov <68978403+ilia-pavlov@users.noreply.github.com>
2023-08-21 22:32:28 -0400

    Update ContentView.swift

--- Peek-a-Pair Pro/ContentView.swift
+++ Peek-a-Pair Pro/ContentView.swift
@@ -8,19 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚂", "🚀", "🚁", "🚕"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            ForEach(emojis, id: \.self) { emoji in
+                CardView(content: emoji)
+            }
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20.0)
+            if isFaceUp {
+                shape.foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape // .fill() by default
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
