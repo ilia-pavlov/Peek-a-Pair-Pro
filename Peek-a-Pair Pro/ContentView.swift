785adeaafc367b048d828b39778b72229cced2c0
ilia Pavlov <68978403+ilia-pavlov@users.noreply.github.com>
2023-08-21 23:30:21 -0400

    Update ContentView.swift

--- Peek-a-Pair Pro/ContentView.swift
+++ Peek-a-Pair Pro/ContentView.swift
@@ -8,16 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = ["🚂", "🚀", "🚁", "🚕"]
+    var emojis = ["✈️", "🚗", "🛳️", "🚆", "🚌",
+                  "🚁", "🚲", "🛴", "🚀", "🛵",
+                  "🚢", "🏍️", "🚂", "🚇", "🛶",
+                  "🚤", "🚍", "🏎️", "🛹", "🛸",
+                  "🛻", "🚟", "🚠", "🚝", "🚄"]
+    @State var emojiCount = 20
     
     var body: some View {
-        HStack {
-            ForEach(emojis, id: \.self) { emoji in
-                CardView(content: emoji)
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
             }
+            .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
@@ -30,7 +68,7 @@ struct CardView: View {
             let shape = RoundedRectangle(cornerRadius: 20.0)
             if isFaceUp {
                 shape.foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                shape.stroke(lineWidth: 3)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content).font(.largeTitle)
             } else {
                 shape // .fill() by default
